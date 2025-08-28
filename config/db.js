243e3d0ed27b410764d59f53c9019c3613130539
Tejpreet12 <tejpreetdhiman429@gmail.com>
2025-08-28 23:36:19 +0530

    fix: correct SQL connection export and update transaction table creation logic

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 const { neon } = require("@neondatabase/serverless");
 
 //Creates Sql connection using our DB URL from .env file
-export const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL);
+
+module.exports = { sql };
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const dotenv = require("dotenv");
-const express = require("express");
 dotenv.config();
+const express = require("express");
+
+const { sql } = require("./config/db");
+
 const app = express();
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
   try {
-    await sql`CREATE TABLE TRANSACTIONS IF NOT EXISTS transactions (
+    await sql`CREATE TABLE IF NOT EXISTS transactions (
                 id SERIAL PRIMARY KEY,  
                 user_id VARCHAR(255) NOT NULL,
                 title VARCHAR(255) NOT NULL,
@@ -17,17 +22,32 @@ async function initDB() {
                 )`;
 
     /**
-     *  Decimal(10, 2) 
-     *  means the number can be up to 
+     *  Decimal(10, 2)
+     *  means the number can be up to
      *  10 digits long, with 2 digits after the decimal point.
-     *  This allows for values ranging from -99999999.99 to 99999999.99.    
-     */            
+     *  This allows for values ranging from -99999999.99 to 99999999.99.
+     */
     console.log("Database initialized successfully");
   } catch (err) {
     console.error("Database connection error:", err);
     process.exit(1); //Status code 1 indicates failure and 0 indicates success
   }
 }
+
+app.post("/api/transactions", async (req, res) => {
+
+  const {user_id,title,category,amount} = req.body;
+   
+  if(!user_id || !title || !category || amount === undefined){
+    return res.status(400).json({error: "Missing required fields"});
+  }
+
+  const transaction = await sql`INSERT INTO transactions (user_id,title,category,amount)
+                                VALUES (${user_id},${title},${category},${amount}) 
+                                RETURNING *`
+
+  res.status(201).json(transaction[0]);
+});
 
 initDB().then(() => {
   app.listen(PORT, () => {
