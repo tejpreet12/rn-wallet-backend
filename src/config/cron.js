7b1d1f339771e8b1259a9e90b032b0d38c0c12a1
tejpreet12 <98548415+tejpreet12@users.noreply.github.com>
2025-09-09 23:03:32 +0530

    Update cron.js

--- src/config/cron.js
+++ src/config/cron.js
@@ -12,7 +12,7 @@ const https = require("https");
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
+      if (res.statusCode === 200) console.log("GET request sent successfully Health Check");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
